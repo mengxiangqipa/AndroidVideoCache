0ab760efd5b0c014b176759dea6386f9ee269d07
YobertJomi <mengxiangqipa@163.com>
2017-09-04 18:21:45 +0800

    2017.9.3

--- Library-AndroidVideoCache/src/main/java/com/danikula/videocache/HttpProxyCache.java
+++ Library-AndroidVideoCache/src/main/java/com/danikula/videocache/HttpProxyCache.java
@@ -39,7 +39,6 @@ class HttpProxyCache extends ProxyCache {
         OutputStream out = new BufferedOutputStream(socket.getOutputStream());
         String responseHeaders = newResponseHeaders(request);
         out.write(responseHeaders.getBytes("UTF-8"));
-
         long offset = request.rangeOffset;
         if (isUseCache(request)) {
             responseWithCache(out, offset);
